86a9fddb6ad9602f0dc18446e2adda3239b9d12d
Roman <krp536@gmail.com>
2021-04-22 17:34:11 +0300

    Update tests

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -2,64 +2,15 @@ import os
 from gendiff.scripts.gendiff import generate_diff
 
 
-def test_generate_diff():
-    test_file1 = os.path.abspath('tests/fixtures/test_file1.json')
-    test_file2 = os.path.abspath('tests/fixtures/test_file2.yml')
-    assert generate_diff(test_file1, test_file2) == \
-        '{\n' \
-        '  - key1: text1\n' \
-        '  - key2: text2\n' \
-        '  + key2: text2_updated\n' \
-        '  + key3: text3\n' \
-        '    key4: text4\n' \
-        '}\n'
-
-
-def test_generate_diff_2():
+def test_stylish_diff():
     test_file1 = os.path.abspath('tests/fixtures/test_file_complex1.json')
     test_file2 = os.path.abspath('tests/fixtures/test_file_complex2.yml')
-    assert generate_diff(test_file1, test_file2) == \
-        '{\n' \
-        '    common: {\n' \
-        '      + follow: False\n' \
-        '        setting1: Value 1\n' \
-        '      - setting2: 200\n' \
-        '      - setting3: True\n' \
-        '      + setting3: None\n' \
-        '      + setting4: blah blah\n' \
-        '      + setting5: {\n' \
-        '            key5: value5\n' \
-        '        }\n' \
-        '        setting6: {\n' \
-        '            doge: {\n' \
-        '              - wow: \n' \
-        '              + wow: so much\n' \
-        '            }\n' \
-        '            key: value\n' \
-        '          + ops: vops\n' \
-        '        }\n' \
-        '    }\n' \
-        '    group1: {\n' \
-        '      - baz: bas\n' \
-        '      + baz: bars\n' \
-        '        foo: bar\n' \
-        '      - nest: {\n' \
-        '            key: value\n' \
-        '        }\n' \
-        '      + nest: str\n' \
-        '    }\n' \
-        '  - group2: {\n' \
-        '        abc: 12345\n' \
-        '        deep: {\n' \
-        '            id: 45\n' \
-        '        }\n' \
-        '    }\n' \
-        '  + group3: {\n' \
-        '        deep: {\n' \
-        '            id: {\n' \
-        '                number: 45\n' \
-        '            }\n' \
-        '        }\n' \
-        '        fee: 100500\n' \
-        '    }\n' \
-        '}\n'
+    with open(os.path.abspath('tests/fixtures/stylish_view')) as result:
+        assert generate_diff(test_file1, test_file2, view='stylish') == result.read()
+
+
+def test_plain_diff():
+    test_file1 = os.path.abspath('tests/fixtures/test_file_complex1.json')
+    test_file2 = os.path.abspath('tests/fixtures/test_file_complex2.yml')
+    with open(os.path.abspath('tests/fixtures/plain_view')) as result:
+        assert generate_diff(test_file1, test_file2, view='plain') == result.read()
