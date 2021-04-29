7559d1e8e1c2390839ddb19b53f9281fe93b4653
Roman <krp536@gmail.com>
2021-04-29 16:24:11 +0300

    Update tests

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -3,17 +3,17 @@ from gendiff.scripts.gendiff import generate_diff
 
 
 def test_stylish_diff():
-    test_file1 = os.path.abspath('tests/fixtures/test_file_complex1.json')
-    test_file2 = os.path.abspath('tests/fixtures/test_file_complex2.yml')
-    with open(os.path.abspath('tests/fixtures/stylish_view')) as result:
-        assert generate_diff(test_file1, test_file2, view='stylish') == result.read()
+    test_file1 = os.path.abspath('tests/fixtures/file1.yml')
+    test_file2 = os.path.abspath('tests/fixtures/file2.yml')
+    with open(os.path.abspath('tests/fixtures/result_stylish')) as result:
+        assert generate_diff(test_file1, test_file2) == result.read()
 
 
 def test_plain_diff():
-    test_file1 = os.path.abspath('tests/fixtures/test_file_complex1.json')
-    test_file2 = os.path.abspath('tests/fixtures/test_file_complex2.yml')
-    with open(os.path.abspath('tests/fixtures/plain_view')) as result:
-        assert generate_diff(test_file1, test_file2, view='plain') == result.read()
+    test_file1 = os.path.abspath('tests/fixtures/file1.yml')
+    test_file2 = os.path.abspath('tests/fixtures/file2.yml')
+    with open(os.path.abspath('tests/fixtures/result_plain')) as result:
+        assert generate_diff(test_file1, test_file2) == result.read()
 
 
 def test_json_diff():
