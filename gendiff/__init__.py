8c92076c8acc2f8f7f12ae21087e6eb70bb2f4a5
Roman <krp536@gmail.com>
2021-04-29 12:44:16 +0300

    Update gendiff's __init__.py

--- gendiff/__init__.py
+++ gendiff/__init__.py
@@ -1,3 +1,4 @@
 from gendiff.scripts import gendiff, internal_diff, parser
+from gendiff.scripts.gendiff import generate_diff
 
-__all__ = ('gendiff', 'internal_diff', 'parser')
+__all__ = ('gendiff', 'internal_diff', 'parser', 'generate_diff')
